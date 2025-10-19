dc310b01df96391baf54dc6c6e2a1ec87c57a671
Andrew Chen <bobopop787@gmail.com>
2025-10-19 14:17:09 -0400

    Begin class TermParser to get current terms from Banner API

---
+++ catalogAPI/src/termParser.js
@@ -0,0 +1,43 @@
+import * as fs from "fs"
+import { bannerURL } from "./constants.js"
+import Cache from "./cache.js"
+
+const currentTermsCacheFilepath = "currentTerms.json"
+
+class TermParser {
+    constructor() {
+        this.cache = new Cache(currentTermsCacheFilepath)
+    }
+
+    async getTerms() {
+        const max = 500
+        const searchTermURL = new URL(`${bannerURL}/classSearch/getTerms`);
+        searchTermURL.searchParams.append("offset", 1);
+        searchTermURL.searchParams.append("max", max);
+
+        const response = await fetch(searchTermURL);
+        const terms = await response.json();
+
+        if(!response.ok) {
+            throw new Error(`Error getting terms from Banner: Status Code ${response.status} - ${response.statusText}`);
+        }
+        return terms;
+    }
+
+    async getCurrentTerms() {
+        const terms = await this.getTerms();
+        const currentTerms = terms.filter(term => !term.description.includes("(View Only)"));
+        return currentTerms;
+    }
+
+    async updateCache() {
+
+    }
+}
+
+// let parser = new TermParser();
+// await parser.updateCache();
+
+const instance = new TermParser();
+
+export default instance;
