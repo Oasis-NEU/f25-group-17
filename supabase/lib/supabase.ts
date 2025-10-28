0c1cd201858412d71c997294fef71f1f451f9c6d
JacksonZheng07 <Jacksonzheng425@gmail.com>
2025-10-27 21:31:32 -0400

    setup the file structure and linked backend

---
+++ supabase/lib/supabase.ts
@@ -0,0 +1,9 @@
+// lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
+
+// These come from your .env.local
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Create a reusable client
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
