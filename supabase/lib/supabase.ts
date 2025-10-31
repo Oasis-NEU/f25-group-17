be2920546ded7382841f837635c1d9e5e8ddee77
JacksonZheng07 <Jacksonzheng425@gmail.com>
2025-10-31 08:59:35 -0400

    added typescript types for the backend

--- supabase/lib/supabase.ts
+++ supabase/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../lib/database.types.ts"
+import type { Database } from "../lib/database.types"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
