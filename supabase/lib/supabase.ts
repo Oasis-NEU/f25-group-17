4024d1319e30470a4fce9d5bdf1ca249f207f1dc
JacksonZheng07 <Jacksonzheng425@gmail.com>
2025-11-02 13:23:49 -0500

    commit

--- supabase/lib/supabase.ts
+++ supabase/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../lib/database.types"
+import type { Database } from "./database.types.ts"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseKey = process.env.NEXT_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
