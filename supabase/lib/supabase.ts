95b860f88e363b3bcfe6c71c5e5c6b2f1e8fa5db
JacksonZheng07 <Jacksonzheng425@gmail.com>
2025-11-04 22:19:58 -0500

    added course catalog to backend

--- supabase/lib/supabase.ts
+++ supabase/lib/supabase.ts
@@ -1,7 +1,12 @@
+import 'dotenv/config';
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types.ts"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
+
+if (!supabaseUrl || !supabaseKey){
+    throw new Error('Missing Supabase environment variables.');
+}
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
