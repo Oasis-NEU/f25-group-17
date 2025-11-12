c62d89b00066603c72265a3df6858ef0e933a269
Andrew Chen <bobopop787@gmail.com>
2025-11-11 19:23:35 -0500

    Update imports correctly from previous refactor

--- catalogAPI/src/parser/termParser.js
+++ catalogAPI/src/parser/termParser.js
@@ -1,5 +1,5 @@
-import { bannerURL } from "./constants.js";
-import Cache from "./cache.js";
+import { bannerURL } from "../constants.js";
+import Cache from "../cache.js";
 
 const currentTermsCacheFilepath = "currentTerms.json";
 
